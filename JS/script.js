409283b009f961675f94a6efbf8cc7868c5890a7
wenjie li <wenjie@wenjies-MacBook-Pro.local>
2019-08-01 16:04:38 -0700

    basic js done

--- JS/script.js
+++ JS/script.js
@@ -4,16 +4,12 @@
 /*------------variables-----------*/
 let playerCards = [];    //an array for player's current cards
 let dealerCards = [];    //an array for dealer's current cards
-let shuffleTimes;        //store the times player wants to shuffle the cards
 let randomIndex;         //an random index for cards array
 let bank;                //hold player current fund total
 let amount;              //the amount player wager
 let choice;              //to store player choice
-let player;              //represent player turn when true
-let dealer;              //represent dealer turn when true
-let playerSum;           //player total number
-let dealerSum;           //dealer total number
-let bust;                //true if bust
+let bust = false;                //true if bust
+let endingDecision;
 
 /*-----------const variables-----*/
 var cards;   //an array for all the cards
@@ -21,7 +17,7 @@ var cards;   //an array for all the cards
 
 /*-----------functions----------*/
 //create cards for the game at the beginning, total 104
-function createCards() {
+function createGame() {
     cards = [
         1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, //from A to K
         1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10,
@@ -32,9 +28,12 @@ function createCards() {
         1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10,
         1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10
     ];
+    shuffleCards();
+    bank = 100;
 }
-//shuffle cards whenever player wants
+//randomly switch array elements position 100 times by default
 function shuffleCards() {
+    let shuffleTimes = 100;
     for(; shuffleTimes > 0; shuffleTimes--){
         let idxOne = getRandomIndex();
         let idxTwo = getRandomIndex();
@@ -53,7 +52,7 @@ function getRandomIndex() {
          return Math.floor(Math.random() * (cards.length - 1));
 }
 //function to lay a bet
-function wager() {
+function layBet() {
     amount = prompt('How much would you like to lay please?');
     bank = bank - amount;
 }
@@ -67,93 +66,184 @@ function calculateTotal(array) {
 }
 //check bust
 function checkBust(array) {
-    if(player === true && dealer === false)
-        playerSum = calculateTotal(array);
-    else if(player === false && dealer === true)
-        dealerSum = calculateTotal(array);
-
-    if(playerSum > 21) {
+    if(calculateTotal(array) > 21) {
+        bust = true;
         return true;
     }else{
         return false;
     }
 }
-//game process
+
+//player turn
 function playerPlay() {
     let steps = 1;
-    player = true;
-    dealer = false;
-    while(!checkBust(playerCards)) {
+    let playerTurn = true;
+    while(playerTurn) {
         if(steps === 1) {
             steps++;
-            choice = prompt('input 1 for hit, 2 for stand, 3 for double');
+            choice = prompt('input hit, double, stand');
         }else {
-            choice = prompt('input 1 for hit, 2 for stand');
+            choice = prompt('input hit, stand');
         }
 
         if(choice === 'hit') {
             getCard(playerCards);
+            console.log(playerCards);
             if(checkBust(playerCards)) {
-                player = false;
-                dealer = true;
-                return;
+                playerTurn = false;
+                break;
             }
-        }else if(choice === 'stand') {
-            player = false;
-            dealer = true;
-            return;
-        }else if(choice === 'double'){
-            bank = bank - amount;
-            amount = amount * 2
-            player = false;
-            dealer = true;
-            return;
         }else{
-            alert('Invalid input! Please try again.');
+            if(choice === 'double'){
+                bank = bank - amount;
+                amount = amount * 2;
+                getCard(playerCards);
+                console.log(playerCards);
+                if(checkBust(playerCards)) {
+                    playerTurn = false;
+                    break;
+                }
+            };
+            playerTurn = false;
         }
     }
 }
+//dealer turn
+function dealerPlay() {
+    let dealerTurn = true;
+    if(checkBust(playerCards)) dealerTurn = false;
+    while(dealerTurn) {
+        if(calculateTotal(dealerCards) < 15){
+            getCard(dealerCards);
+            if(checkBust(dealerCards)){
+                dealerTurn = false;
+                break;
+            };
+        }
+        else{
+            dealerTurn = false;
+            return;
+        }
+    }
+}
+
+//compare result
+function compareBoth() {
+    let playerSum = calculateTotal(playerCards);
+    let dealerSum = calculateTotal(dealerCards);
+    console.log('player total: '+playerSum);
+    console.log('dealer total: '+ dealerSum);
+
+    if(playerSum > 21){
+        console.log(`Dealer Win.`);
+        amount = 0;
+        console.log('bank is: '+bank + ', amount is :' + amount);
+        return;
+    }else if(dealerSum > 21){
+        console.log(`Player Win.`);
+        amount = amount * 2;
+        bank = bank + amount;
+        amount = 0;
+        console.log('bank is: '+bank + ', amount is :' + amount);
+        return;
+    }else{
+        if(playerSum > dealerSum) {
+            console.log(`Player Win.`);
+            amount = amount * 2;
+            bank = bank + amount;
+            amount = 0;
+            console.log('bank is: '+bank + ', amount is :' + amount);
+            return;
+        }else if(playerSum < dealerSum) {
+            console.log(`Dealer Win.`);
+            amount = 0;
+            console.log('bank is: '+bank + ', amount is :' + amount);
+            return;
+        }else{
+            console.log(`Tie.`);
+            bank = bank + amount;
+            amount = 0;
+            consofle.log('bank is: '+bank + ', amount is :' + amount);
+            return;
+        }
+    };
+}
+
+//end
+function nextRound() {
+    if(bank === 0) {
+        endingDecision = prompt('no to exit, again to start over');
+    }else {
+        endingDecision = prompt('yes for next round, no to exit, again to start over, replay will be disable if fund = 0');
+    };
+    if(endingDecision === 'again') {
+        createGame();
+    };
+}
+
 //call to start game
-
-
-
-
-
+function startGame() {
+    //player to lay
+    layBet();
+    console.log('bank:'+bank+' amount: '+ amount);
+    //empty player and dealer cards
+    playerCards = [];
+    dealerCards = [];
+    //player to get initial cards
+    getCard(playerCards);
+    getCard(playerCards);
+    console.log('player cards '+playerCards);
+    console.log('cards length: '+cards.length);
+    //dealer get cards
+    getCard(dealerCards);
+    getCard(dealerCards);
+    console.log('cards length: '+cards.length);
+}
 
 //--------------------------------------test
-//create cards
-createCards();
+
+//create cards and shuffle cards,assign initial fund
+createGame();
 console.log('origin:' +cards);
-//shuffle cards
-shuffleTimes = prompt(`how many times?`);
-shuffleCards();
 console.log('cards after shuffle: '+cards);
 console.log('cards length: '+cards.length);
-//player to get initial money
-bank = 100;
+console.log('--------------------game been created---------');
+
 //game start
-//player to lay
-wager();
-console.log('bank:'+bank+' amount: '+ amount);
-//player to get initial cards
-getCard(playerCards);
-getCard(playerCards);
-console.log('player cards '+playerCards);
-console.log('cards length: '+cards.length);
-//dealer get cards
-getCard(dealerCards);
-getCard(dealerCards);
-console.log('dealer cards '+dealerCards);
-console.log('cards length: '+cards.length);
-//player first turn
-playerPlay();
-console.log('after playerplay');
-console.log(cards);
-console.log(playerCards);
-console.log(dealerCards);
-console.log(bank);
-console.log(amount);
-console.log(playerSum);
+do{
+    startGame();
+    console.log('--------------------game start---------');
+
+    //playing
+            //player turn
+            playerPlay();
+            console.log('after playerplay');
+            console.log('remaining cards: '+cards);
+            console.log('player cards :'+playerCards);
+            console.log('remaining fund ' +bank);
+            console.log('bet amount:' +amount);
+            console.log('--------------------player turn done---------');
+                    
+                    //dealer turn
+                    if(bust === false){
+                        dealerPlay();
+                        console.log('after dealerplay');
+                        console.log('remaining cards: ' + cards);
+                        console.log('player cards :'+playerCards);
+                        console.log('dealer cards:' +dealerCards);
+                        console.log('--------------------dealer turn done---------');
+                    };
+    console.log('---------------------game done---------');
+    //compare
+    compareBoth();
+    console.log('--------------------compare done---------');
+
+
+    //end
+    console.log('--------------------next round??---------');
+    nextRound();
+}while(endingDecision !== 'no');
+
 
 
 
